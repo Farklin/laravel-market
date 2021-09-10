54a7bfe5267259051c5d7e64625f743353d7ece3
Farklin <XXXlinkoln@yandex.ru>
2021-09-10 16:31:42 +0300

    sidebar

--- resources/views/layouts/left_sidebar.blade.php
+++ resources/views/layouts/left_sidebar.blade.php
@@ -1,23 +1,20 @@
 <div class="catagories-menu">
     <ul id="menu-content2" class="menu-content collapse show">
         <!-- Single Item -->
-        @foreach($header_category as $category)
-        <li data-toggle="collapse" data-target="#clothing">
-            <a href="{{route('category', $category->seo->slug )}}">{{ $category->title }}</a>
-            {{-- <ul class="sub-menu collapse show" id="clothing">
-                                        <li><a href="#">All</a></li>
-                                        <li><a href="#">Bodysuits</a></li>
-                                        <li><a href="#">Dresses</a></li>
-                                        <li><a href="#">Hoodies &amp; Sweats</a></li>
-                                        <li><a href="#">Jackets &amp; Coats</a></li>
-                                        <li><a href="#">Jeans</a></li>
-                                        <li><a href="#">Pants &amp; Leggings</a></li>
-                                        <li><a href="#">Rompers &amp; Jumpsuits</a></li>
-                                        <li><a href="#">Shirts &amp; Blouses</a></li>
-                                        <li><a href="#">Shirts</a></li>
-                                        <li><a href="#">Sweaters &amp; Knits</a></li>
-                                    </ul> --}}
-        </li>
+        @foreach($header_category as $title_category)
+            @if($title_category->category_id == 0)
+                <li data-toggle="collapse" data-target="#clothing">
+                    <a href="{{route('category', $title_category->seo->slug )}}">{{ $title_category->title }}</a>
+                    @if(isset($title_category->category))
+                    
+                    <ul class="sub-menu collapse show" id="clothing">
+                        @foreach($title_category->category as $child_category)
+                        <li><a href="{{ route('category', $child_category->seo->slug) }}">{{ $child_category->title }}</a></li> 
+                        @endforeach
+                    </ul>
+                    @endif 
+                </li>
+            @endif 
         @endforeach
         {{-- <!-- Single Item -->
                                 <li data-toggle="collapse" data-target="#shoes" class="collapsed">

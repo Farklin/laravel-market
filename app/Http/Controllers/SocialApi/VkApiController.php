de28d19a1f7892ac87156ab2f3ed63ec06765ac6
Farklin <XXXlinkoln@yandex.ru>
2021-09-13 12:29:13 +0300

    api fix

--- app/Http/Controllers/SocialApi/VkApiController.php
+++ app/Http/Controllers/SocialApi/VkApiController.php
@@ -8,32 +8,51 @@ use Atehnix\VkClient\Src\Client;
 
 class VkApiController extends Controller
 {
+    public static function secret_code(){
+        
 
+        // 1. инициализация
+        $ch = curl_init();
+
+        // 2. указываем параметры, включая url
+        curl_setopt($ch, CURLOPT_URL, "https://oauth.vk.com/authorize?client_id=7948916&display=page
+        &redirect_uri=https://api.vk.com/blank.html&scope=offline,wall,photos
+        &response_type=code");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+
+        // 3. получаем HTML в качестве результата
+        $output = curl_exec($ch);
+
+        // 4. закрываем соединение
+        curl_close($ch);
+    }
     public function post(){
 
-        // https://oauth.vk.com/authorize?client_id=7948916&scope=groups,wall,offline,photos&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token
-        
-        $params = array(
-            'v' => '5.131', // версия вк апи
+            $array = array(
+                'v' => '5.131', // версия вк апи
             'scope' => 'wall,photos,video', // доступ к стене,фото,видео
             'access_token' => 'ff2953281ad49430293d5bbff9ee95d3d5f2d7088c50b451688391ad599a52945ae459809997878449474', // тут ваш access_token
             'owner_id' => '672366606', // ID ГРУППЫ С ОБЯЗАТЕЛЬНЫМ ЗНАКОМ "-" ПЕРЕД ЧИСЛОМ!
             'attachments' => 'https://teisbubble.ru/', // ссылка на вашу страницу, которая будет расшарена на вашей стене группы
             'message' => 'Любой ваш текст' // тут текст который будет в карточке расшаривания, если оставить пустым то будет дублировать title со страницы в параметре attachments
             );
-                
-                
-            $url = 'https://api.vk.com/method/wall.post?' . http_build_query($params);
-            
-            $ch = curl_init($url);
+             
+            $ch = curl_init('https://api.vk.com/method/wall.post?');
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $array); 
+             
+            // Или предать массив строкой: 
+            // curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($array, '', '&'));
+             
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            $response = curl_exec($ch);
-            $result = curl_getinfo($ch , CURLINFO_HTTP_CODE); 
-            curl_close($ch);
-    
-            return $result; 
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            $html = curl_exec($ch);
+            curl_close($ch);	
+             
+            return $html;
+
     
     }
     

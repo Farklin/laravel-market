bdb6eb97004b127ee5283be3ced7c3e0c5f2574b
Farklin <XXXlinkoln@yandex.ru>
2021-09-14 12:55:48 +0300

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -60,10 +60,12 @@
       <div class="row mt-5">
           <div class="col-md-12 text-center">
               <p>
-                  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> TeisBubble <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="{{ route('home') }}" target="_blank">Colorlib</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> TeisBubble </a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
               </p>
+              <p>
+Разработка сайта: <a href="https://vk.com/ivanhel" style="color:white" target="_blank">Иван Жильцов</a>
+            </p>
           </div>
       </div>
       

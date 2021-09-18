f50b4e287cd51f8e6f291b6e243386490d6941fa
Farklin <67648571+Farklin@users.noreply.github.com>
2021-09-18 20:28:03 +0400

    Изменение карточки товара в каталоге

--- resources/views/catalog/product/card_product.blade.php
+++ resources/views/catalog/product/card_product.blade.php
@@ -1,6 +1,5 @@
 <div class="col-12 col-sm-6 col-lg-4">
     <div class="single-product-wrapper">
-        <!-- Product Image -->
         <a href="{{ route('product.show', $product->seo->slug) }}"> 
         <div class="product-img">
 
@@ -8,7 +7,6 @@
                 <img src="{{ $product->images[0]->thumbnail() }}" alt="">
             @endif
             
-            <!-- Hover Thumb -->
 
             @if(isset($product->images[1])) 
                 <img class="hover-img" src="{{ $product->images[1]->thumbnail() }}" alt="">
@@ -17,19 +15,16 @@
         </div>
         </a> 
 
-        <!-- Product Description -->
         <div class="product-description">
-            <span>topshop</span>
+            <span></span>
+            <div class="product-price h5">{{ $product->price }} ₽</div>
             <a href="{{ route('product.show', $product->seo->slug)}} ">
                 <h6>{{ $product->title }}</h6>
             </a>
-            <p class="product-price">{{ $product->price }} </p>
+           
 
-            <!-- Hover Content -->
             <div class="hover-content">
-                <!-- Add to Cart -->
                 <div class="add-to-cart-btn">
-                     <!-- Form -->
                      <form class="cart-form clearfix" action="{{ route('basket.add', ['id' => $product->id]) }}" method="post">  
                         @csrf 
                     <input type="hidden" name="quantity" id="input-quantity" value="1" class="form-control mx-2 w-25">

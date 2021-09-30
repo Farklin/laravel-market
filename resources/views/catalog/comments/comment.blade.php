9d73597f87146e4b855146c00b61902b6f339666
boonboks <XXXlinkoln@yandex.ru>
2021-09-30 22:36:21 +0300

    comment

---
+++ resources/views/catalog/comments/comment.blade.php
@@ -0,0 +1,11 @@
+<div class="comment">
+    <div class="row comment-name">
+        
+    </div>
+    <div class="row comment-text">
+        <span> </span> 
+    </div>
+    <div class="row comment-rating">
+        
+    </div>
+</div>

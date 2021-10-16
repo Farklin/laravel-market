1bfe2b366b00c01e3042da4b0a0e406bd8680047
Farklin <67648571+Farklin@users.noreply.github.com>
2021-10-16 10:13:26 +0400

    Исправление вывода товаров

--- public/theme/app/app-blog-new-post.1.1.0.js
+++ public/theme/app/app-blog-new-post.1.1.0.js
@@ -43,13 +43,6 @@
         return true; // submit form
     }
 
-    var form = document.getElementById("Form"); // get form by ID
-
-    form.onsubmit = function() { // onsubmit do this first
-
-        $("#description").val($('#editor-container').html());
-        return true; // submit form
-    }
 
 
 })(jQuery);

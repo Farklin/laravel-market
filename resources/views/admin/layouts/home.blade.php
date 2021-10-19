a952cf12753147d0904528034ef154db23f9698e
boonboks <ivanhel@yandex.ru>
2021-10-19 16:10:15 +0300

    Исправление отправки отзыва

--- resources/views/admin/layouts/home.blade.php
+++ resources/views/admin/layouts/home.blade.php
@@ -296,7 +296,7 @@
                                 @foreach($comments as $comment)
                                     <div class="blog-comments__item d-flex p-3">
                                         <div class="blog-comments__avatar mr-3">
-                                            @if( count( $comment->product->image ) > 0 )
+                                            @if( isset( $comment->product->image ) > 0 )
                                             <img src="{{ $comment->product->images[0]->thumbnail() }}"/>
                                             @endif
                                         </div>
